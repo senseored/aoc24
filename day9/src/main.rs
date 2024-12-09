5037c45e1da40ac39e48ea3c2339bb94d15b2161
senseored <tobiasgus@gmail.com>
2024-12-09 07:05:01 +0100

    Day 9 init

---
+++ day9/src/main.rs
@@ -0,0 +1,12 @@
+use std::fs;
+
+fn main() {
+    // let file_path = "input/test2.txt";
+    // let file_path = "input/test.txt";
+    let file_path = "input/day9.txt";
+    // println!("In file {file_path}");
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("{contents}");
+}
