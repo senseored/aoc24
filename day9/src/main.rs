a15e7f178ebb73becdb9800d4e679123cef42675
senseored <tobiasgus@gmail.com>
2024-12-09 16:50:03 +0100

    Day 9 part 1 done

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -20,7 +20,9 @@ fn main() {
     let mut sortmap_rev: String = String::new();
     let mut i = 0;
     let mut vecmap: Vec<i32> = Vec::new();
+    let mut vecmap2: Vec<i32> = Vec::new();
     let mut sortvecmap: Vec<i32> = Vec::new();
+    let mut sortvecmap2: Vec<i32> = Vec::new();
     let mut revvecmap: Vec<i32> = Vec::new();
     let mut maplen: usize = 0;
 
@@ -37,11 +39,13 @@ fn main() {
             for _x in 0..c as i8 - 48 {
                 // map = format!("{}{}", map, i / 2);
                 vecmap.push(i / 2);
+                vecmap2.push(i / 2);
             }
         } else {
             for _x in 0..c as i8 - 48 {
                 // map = format!("{}{}", map, ".");
                 vecmap.push(-1);
+                freevec += 1;
             }
         }
         i += 1;
@@ -55,69 +59,79 @@ fn main() {
     // println!("{}, {}", map, map.len());
     // println!("{:?}", vecmap);
     sortmap_rev = map.clone();
-    while sortmap.len() + freespace < map.len() {
-        map.chars().for_each(|c| {
-            if sortmap.len() + freespace < map.len() {
-                if c.is_digit(10) {
-                    sortmap = format!("{}{}", sortmap, c);
-                } else {
-                    sortmap_rev = format!("{}{}", sortmap_rev, c);
-                    while sortmap_rev.chars().last().unwrap() == '.' {
-                        freespace += 1;
-                        sortmap_rev.pop();
-                    }
-                    sortmap = format!("{}{}", sortmap, sortmap_rev.chars().last().unwrap());
-                    sortmap_rev.pop();
-                }
-            }
-        });
-    }
+    // while sortmap.len() + freespace < map.len() {
+    //     map.chars().for_each(|c| {
+    //         if sortmap.len() + freespace < map.len() {
+    //             if c.is_digit(10) {
+    //                 sortmap = format!("{}{}", sortmap, c);
+    //             } else {
+    //                 sortmap_rev = format!("{}{}", sortmap_rev, c);
+    //                 while sortmap_rev.chars().last().unwrap() == '.' {
+    //                     freespace += 1;
+    //                     sortmap_rev.pop();
+    //                 }
+    //                 sortmap = format!("{}{}", sortmap, sortmap_rev.chars().last().unwrap());
+    //                 sortmap_rev.pop();
+    //             }
+    //         }
+    //     });
+    // }
     revvecmap = vecmap.clone();
+    println!("{:?}", vecmap);
     vecmap.iter().for_each(|n| {
-        println!(
-            "sortvevclen: {}, freevec: {}, vecmaplen, {}",
-            sortvecmap.len(),
-            freevec,
-            vecmap.len()
-        );
-        if sortvecmap.len() + freevec < vecmap.len() {
+        if sortvecmap2.len() + freevec < vecmap.len() {
             if !n.is_negative() {
-                sortvecmap.push(*n);
+                sortvecmap2.push(*n);
             } else {
-                revvecmap.push(*n);
-                while revvecmap.last().unwrap().is_negative() {
-                    freevec += 1;
-                    revvecmap.pop();
-                }
-                sortvecmap.push(*revvecmap.last().unwrap());
-                revvecmap.pop();
+                sortvecmap2.push(*vecmap2.last().unwrap());
+                vecmap2.pop();
             }
-            // println!("n: {}", n);
-            // println!(
-            //     "sortvevclen: {}, freevec: {}, vecmaplen, {}",
-            //     sortvecmap.len(),
-            //     freevec,
-            //     vecmap.len()
-            // );
-            // if *n == -1 {
-            //     // freevec += 1;
-            //     // while revvecmap[revvecmap.len() - 1] != -1 {
-            //     while *revvecmap.last().unwrap() == -1 {
-            //         freevec += 1;
-            //         revvecmap.pop();
-            //     }
-            //     sortvecmap.push(*revvecmap.last().unwrap());
-            //     revvecmap.pop();
-            // } else {
-            //     sortvecmap.push(*n);
-            // }
         }
+        // println!(
+        //     "sortvevclen: {}, freevec: {}, vecmaplen, {}",
+        //     sortvecmap.len(),
+        //     freevec,
+        //     vecmap.len()
+        // );
+        // if sortvecmap.len() + freevec < vecmap.len() {
+        //     if !n.is_negative() {
+        //         sortvecmap.push(*n);
+        //     } else {
+        //         revvecmap.push(*n);
+        //         while revvecmap.last().unwrap().is_negative() {
+        //             freevec += 1;
+        //             revvecmap.pop();
+        //         }
+        //         sortvecmap.push(*revvecmap.last().unwrap());
+        //         revvecmap.pop();
+        //     }
+        // println!("n: {}", n);
+        // println!(
+        //     "sortvevclen: {}, freevec: {}, vecmaplen, {}",
+        //     sortvecmap.len(),
+        //     freevec,
+        //     vecmap.len()
+        // );
+        // if *n == -1 {
+        //     // freevec += 1;
+        //     // while revvecmap[revvecmap.len() - 1] != -1 {
+        //     while *revvecmap.last().unwrap() == -1 {
+        //         freevec += 1;
+        //         revvecmap.pop();
+        //     }
+        //     sortvecmap.push(*revvecmap.last().unwrap());
+        //     revvecmap.pop();
+        // } else {
+        //     sortvecmap.push(*n);
+        // }
+        // }
     });
-    sortvecmap.iter().for_each(|n| {
+    sortvecmap2.iter().for_each(|n| {
         if n.is_negative() {
             println!("yo i done messed up");
         }
     });
+    println!("{:?}", sortvecmap2);
     // revvecmap.pop();
     // sortvecmap.push(*revvecmap.last().unwrap());
     // revvecmap.pop();
@@ -145,7 +159,7 @@ fn main() {
     let mut sum: i128 = 0;
     let mut count2 = 0;
     let mut count: i128 = 0;
-    sortvecmap.iter().for_each(|n| {
+    sortvecmap2.iter().for_each(|n| {
         // println!("n: {}, sum: {}, count: {}", n, sum, count);
         if count2 < maplen {
             sum += *n as i128 * count;
@@ -182,4 +196,5 @@ fn main() {
     //6283665299144
     //24071685531123
     //24071685531123
+    //6283404590840
 }
