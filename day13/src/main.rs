756e6684916e8b4c058b0bd7f623d06a07bd1b26
senseored <tobiasgus@gmail.com>
2024-12-13 14:17:21 +0100

    Day 13 init

---
+++ day13/src/main.rs
@@ -0,0 +1,14 @@
+
+use std::collections::HashMap;
+use std::fs;
+
+fn main() {
+    let file_path = "input/test.txt";
+    // let file_path = "input/day13.txt";
+
+    // println!("In file {file_path}");
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("{contents}");
+}
